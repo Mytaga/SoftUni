812695fc65a465e010a51a26b4629491da50846c
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-01-10 21:54:57 +0200

    Add files via upload

---
+++ Exams/OOPExam 11-12-2021/Gym/Core/Controller.cs
@@ -0,0 +1,153 @@
+﻿using Gym.Core.Contracts;
+using Gym.Models.Athletes;
+using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms;
+using Gym.Models.Gyms.Contracts;
+using Gym.Repositories;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Core
+{
+    public class Controller : IController
+    {
+        private readonly EquipmentRepository equipment;
+        private readonly List<IGym> gyms;
+
+        public Controller()
+        {
+            this.equipment = new EquipmentRepository();
+            this.gyms = new List<IGym>();
+        }
+
+        public string AddAthlete(string gymName, string athleteType, string athleteName, string motivation, int numberOfMedals)
+        {
+            IAthlete athlete = null;
+
+            if (athleteType == "Boxer")
+            {
+                athlete = new Boxer(athleteName, motivation, numberOfMedals);
+            }
+            else if (athleteType == "Weightlifter")
+            {
+                athlete = new Weightlifter(athleteName, motivation, numberOfMedals);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidAthleteType);
+            }
+
+            var gym = this.gyms.FirstOrDefault(g => g.Name == gymName);
+
+            if (gym.GetType().Name == "BoxingGym" && athleteType == "Weightlifter")
+            {
+                return OutputMessages.InappropriateGym;
+            }
+            else if (gym.GetType().Name == "WeightliftingGym" && athleteType == "Boxer")
+            {
+                return OutputMessages.InappropriateGym;
+            }
+            else
+            {
+                gym.AddAthlete(athlete);
+                return string.Format(OutputMessages.EntityAddedToGym, athleteType, gymName);
+            }
+        }
+
+        public string AddEquipment(string equipmentType)
+        {
+            IEquipment equipment = null;
+
+            if (equipmentType == "BoxingGloves")
+            {
+                equipment = new BoxingGloves();
+            }
+            else if (equipmentType == "Kettlebell")
+            {
+                equipment = new Kettlebell();
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidEquipmentType);
+            }
+
+            this.equipment.Add(equipment);
+            return string.Format(OutputMessages.SuccessfullyAdded, equipmentType);
+        }
+
+        public string AddGym(string gymType, string gymName)
+        {
+            IGym gym = null;
+
+            if (gymType == "BoxingGym")
+            {
+                gym = new BoxingGym(gymName);
+            }
+            else if (gymType == "WeightliftingGym")
+            {
+                gym = new WeightliftingGym(gymName);
+            }
+            else
+            {
+                throw new InvalidOperationException(ExceptionMessages.InvalidGymType);
+            }
+
+            this.gyms.Add(gym);
+            return string.Format(OutputMessages.SuccessfullyAdded, gymType);
+        }
+
+        public string EquipmentWeight(string gymName)
+        {
+            var gym = this.gyms.FirstOrDefault(g => g.Name == gymName);
+            double totalWeight = gym.EquipmentWeight;
+
+            return $"The total weight of the equipment in the gym {gymName} is {totalWeight:f2} grams.";
+        }
+
+        public string InsertEquipment(string gymName, string equipmentType)
+        {
+            var equipment = this.equipment.FindByType(equipmentType);
+
+            if (equipment == null)
+            {
+                throw new InvalidOperationException(string.Format(ExceptionMessages.InexistentEquipment, equipmentType));
+            }
+
+            var gym = this.gyms.FirstOrDefault(g => g.Name == gymName);
+
+            gym.AddEquipment(equipment);
+            this.equipment.Remove(equipment);
+
+            return string.Format(OutputMessages.EntityAddedToGym, equipmentType, gymName);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var gym in gyms)
+            {
+                sb.AppendLine(gym.GymInfo());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        public string TrainAthletes(string gymName)
+        {
+            var gym = this.gyms.FirstOrDefault(g => g.Name == gymName);
+
+            foreach (var athlete in gym.Athletes)
+            {
+                athlete.Exercise();
+            }
+
+            return string.Format(OutputMessages.AthleteExercise, gym.Athletes.Count);
+        }
+    }
+}
---
+++ Exams/OOPExam 11-12-2021/Gym/Models/Gyms/Gym.cs
@@ -0,0 +1,95 @@
+﻿using Gym.Models.Athletes.Contracts;
+using Gym.Models.Equipment.Contracts;
+using Gym.Models.Gyms.Contracts;
+using Gym.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Gym.Models.Gyms
+{
+    public abstract class Gym : IGym
+    {
+        private string name;
+        private readonly List<IAthlete> athletes;
+        private readonly List<IEquipment> equipment;
+
+        protected Gym(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            this.equipment = new List<IEquipment>();
+            this.athletes = new List<IAthlete>();
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.InvalidGymName);
+                }
+
+                this.name = value;
+            }
+        }
+
+        public int Capacity { get; }
+
+        public double EquipmentWeight => this.Equipment.Sum(e => e.Weight);
+
+        public ICollection<IEquipment> Equipment => this.equipment;
+
+        public ICollection<IAthlete> Athletes => this.athletes;
+
+        public void AddAthlete(IAthlete athlete)
+        {
+            if (this.Athletes.Count >= this.Capacity)
+            {
+                throw new InvalidOperationException(ExceptionMessages.NotEnoughSize);
+            }
+
+            this.athletes.Add(athlete);
+        }
+
+        public void AddEquipment(IEquipment equipment)
+        {
+            this.equipment.Add(equipment);
+        }
+
+        public void Exercise()
+        {
+            foreach (var athlete in this.Athletes)
+            {
+                athlete.Exercise();
+            }
+        }
+
+        public string GymInfo()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.Name} is a {this.GetType().Name}:");
+            if (Athletes.Count == 0)
+            {
+                sb.AppendLine("Athletes: No athletes");
+            }
+            else
+            {
+                sb.AppendLine($"Athletes: {string.Join(", ", this.Athletes)}");
+            }
+
+            sb.AppendLine($"Equipment total count: {this.Equipment.Count}");
+            sb.AppendLine($"Equipment total weight: {this.EquipmentWeight:f2} grams");
+
+            return sb.ToString().TrimEnd();
+        }   
+
+        public bool RemoveAthlete(IAthlete athlete)
+        {
+            return this.athletes.Remove(athlete);
+        }
+    }
+}
