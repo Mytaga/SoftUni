c6af5db6685aad3cbe3faf338c74e0c976574799
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-05 17:04:04 +0300

    Add files via upload

---
+++ ASP.NET Advanced Final Project/BulgariaEstates/BulgariaEstates.Common/GlobalConstants.cs
@@ -0,0 +1,10 @@
+﻿namespace BulgariaEstates.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "BulgariaEstates";
+
+        public const string AdministratorRoleName = "Administrator";
+        public const string UserRoleName = "User";
+    }
+}
