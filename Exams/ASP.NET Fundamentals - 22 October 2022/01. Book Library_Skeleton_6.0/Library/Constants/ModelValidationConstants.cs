82088dd05b9df108cef6df9cf83b67473fabb985
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-23 15:10:38 +0300

    Add files via upload

---
+++ Exams/ASP.NET Fundamentals - 22 October 2022/01. Book Library_Skeleton_6.0/Library/Constants/ModelValidationConstants.cs
@@ -0,0 +1,51 @@
+﻿namespace Library.Constants
+{
+    public static class ModelValidationConstants
+    {
+        public const int BookTitleMinLength = 10;
+        public const int BookTitleMaxLength = 50;
+        public const int BookAuthorMinLength = 5;
+        public const int BookAuthorMaxLength = 50;
+        public const int BookDescriptionMinLength = 5;
+        public const int BookDescriptionMaxLength = 5000;
+        public const string BookRatingMinValue = "0.00";
+        public const string BookRatingMaxValue = "10.00";
+
+        public const int CategoryNameMaxLength = 50;
+
+        public const int RegisterUserNameMinLength = 5;
+        public const int RegisterUserNameMaxLength = 20;
+
+        public const int RegisterEmailMinLength = 10;
+        public const int RegisterEmailMaxLength = 60;
+
+        public const int RegisterPasswordMinLength = 5;
+        public const int RegisterPasswordMaxLength = 20;
+
+        public const string BookTitleRequiredError = "Title is required";
+        public const string BookTitleMinLengthError = "Title must be at least 10 symbols";
+        public const string BookTitleMaxLengthError = "Title must be no more than 50 symbols";
+        public const string BookAuthorRequiredError = "Author is required";
+        public const string BookAuthorMinLengthError = "Author must be at least 5 symbols";
+        public const string BookAuthorMaxLengthError = "Author must be no more than 50 symbols";
+        public const string BookDescriptionRequiredError = "Description is required";
+        public const string BookDescriptionMinLengthError = "Description must be at least 5 symbols";
+        public const string BookDescriptionMaxLengthError = "Description must be no more than 5000 symbols";
+        public const string BookImageUrlRequiredError = "Image URL is required";
+        public const string BookRatingRequiredError = "Rating is required";
+        public const string BookRatingRangeError = "Rating must be between 0.00 and 10.00";
+       
+
+        public const string RegisterUserNameRequiredError = "Username is required";
+        public const string RegisterUserNameMinLengthError = "Username must be at least 5 symbols";
+        public const string RegisterUserNameMaxLengthError = "Username must be no more than 20 symbols";
+        public const string RegisterEmailRequiredError = "Email is required";
+        public const string RegisterEmailMinLengthError = "Email must be at least 10 symbols";
+        public const string RegisterEmailMaxLengthError = "Email must be no more than 60 symbols";
+        public const string RegisterPasswordRequiredError = "Password is required";
+        public const string RegisterPasswordMinLengthError = "Password must be at least 5 symbols";
+        public const string RegisterPasswordMaxLengthError = "Password must be no more than 20 symbols";
+
+        public const string Username = "Username";
+    }
+}
---
+++ Exams/ASP.NET Fundamentals - 22 October 2022/01. Book Library_Skeleton_6.0/Library/Controllers/UserController.cs
@@ -0,0 +1,113 @@
+﻿using Library.Data.Models;
+using Library.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Library.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserManager<ApplicationUser> userManager;
+        private readonly SignInManager<ApplicationUser> signInManager;
+
+        public UserController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager)
+        {
+            this.userManager = userManager;
+            this.signInManager = signInManager;
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Register()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return this.RedirectToAction("All", "Books");
+            }
+
+            var model = new RegisterViewModel();
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var user = new ApplicationUser()
+            {
+                Email = model.Email,
+                UserName = model.UserName,
+            };
+
+            var result = await this.userManager.CreateAsync(user, model.Password);
+
+            if (result.Succeeded)
+            {
+                return this.RedirectToAction("Login", "User");
+            }
+
+            foreach (var item in result.Errors)
+            {
+                this.ModelState.AddModelError("", item.Description);
+            }
+
+            return this.View(model);
+        }
+
+        [HttpGet]
+        [AllowAnonymous]
+        public IActionResult Login()
+        {
+            if (User?.Identity?.IsAuthenticated ?? false)
+            {
+                return this.RedirectToAction("All", "Movies");
+            }
+
+            var model = new LoginViewModel();
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public async Task<IActionResult> Login(LoginViewModel model)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                return this.View(model);
+            }
+
+            var user = await this.userManager.FindByNameAsync(model.UserName);
+
+            if (user != null)
+            {
+                var result = await this.signInManager.PasswordSignInAsync(user, model.Password, false, false);
+
+                if (result.Succeeded)
+                {
+                    return this.RedirectToAction("All", "Books");
+                }
+            }
+
+            this.ModelState.AddModelError("", "Invalid login");
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Logout()
+        {
+            await this.signInManager.SignOutAsync();
+
+            return this.RedirectToAction("Index", "Home");
+        }
+
+    }
+}
