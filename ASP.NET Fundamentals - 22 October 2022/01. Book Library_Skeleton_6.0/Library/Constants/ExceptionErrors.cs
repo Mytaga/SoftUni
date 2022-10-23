72c6e091b6917fc0c07a89e812779bd69b598d1d
Dimitar Kralev <86414839+Mytaga@users.noreply.github.com>
2022-10-23 15:08:02 +0300

    Delete ASP.NET Fundamentals - 22 October 2022 directory

--- ASP.NET Fundamentals - 22 October 2022/01. Book Library_Skeleton_6.0/Library/Constants/ExceptionErrors.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Library.Constants
-{
-    public static class ExceptionErrors
-    {
-        public const string InvalidUser = "Invalid user ID";
-        public const string InvalidBook = "Invalid book ID";
-    }
-}
